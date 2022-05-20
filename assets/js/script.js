3be69f30d10cc18f25527db7ffb014afd0215167
auliiaasr <89631647+auliiaasr@users.noreply.github.com>
2022-05-20 13:37:37 +0700

    2

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     $('#load-film').click(function () {
         $.get("film.php?page=" + page, function (respon) {
             $.each(respon, function (key, value) {
-                $('#film').append('<div class="col d-flex justify-content-center mb-4"><div class="card shadow zoom"> <img src="http://www.upnorthgeorgia.org/wp-content/uploads/2014/09/holes.jpg" class="card-img-top" alt="' + value.title + '"> <div class="card-body"><h5 class="card-title text-uppercase">' + value.title + '</h5><div class="d-flex justify-content-between"><p class="card-text">Rating: ' + value.rating + '</p><div><ul><li class="nav-item mx-2 d-inline"><a href="#"><i class="bi bi-pencil-square edit"></i></a></li><li class="nav-item d-inline"><a href="#"><i class="bi bi-trash hapus text-danger"></i></a></li></ul></div></div></div></div></div>');
+                $('#film').append('<div class="col d-flex justify-content-center mb-4"><div class="card shadow zoom"> <img src="https://altfilmlens.files.wordpress.com/2015/11/the_good_dinosaur_promo_art_03.jpg" class="card-img-top" alt="' + value.title + '"> <div class="card-body"><h5 class="card-title text-uppercase">' + value.title + '</h5><div class="d-flex justify-content-between"><p class="card-text">Rating: ' + value.rating + '</p><div><ul><li class="nav-item mx-2 d-inline"><a href="#"><i class="bi bi-pencil-square edit"></i></a></li><li class="nav-item d-inline"><a href="#"><i class="bi bi-trash hapus text-danger"></i></a></li></ul></div></div></div></div></div>');
             });
             page += 12;
         });
